8fa7b008349fa0e0144433e42256883ac60e9055
Innocenzo Tremamondo <innocenzo@host-166.istitutolanteri.it>
2013-05-12 15:20:17 +0200

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Innocenzo Tremamondo on 12/05/13.
+//  Copyright (c) 2013 Innocenzo Tremamondo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
